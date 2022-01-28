01eb85d65b00e1d2aa6cbc3838387e2f0d12aa73
Your Name <selvakaran9176@gmail.com>
2022-01-28 14:16:48 +0530

    pushed everthing to check

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,7 @@
 
 // navbar hiding while scrolling and  closing search while scrolling
+
+function init()
 {
     const navBar = document.querySelector(".header"); 
     let lastScrollY = window.scrollY;
@@ -22,7 +24,7 @@
 
             lastScrollY = window.scrollY;
     });
-    }
+    
 
     //search button 
 
@@ -32,21 +34,27 @@
     const cancelBtn = document.querySelector(".cancel");
     const searchData = document.querySelector(".search-data");
 
-    {
+    const bikes = [ {bikeName:"A", price:100}, {bikeName:"B", price:200}]
+    
         searchBtn.onclick = () =>{
             searchBox.classList.add("active");
             searchInput.classList.add("active");
             searchBtn.classList.add("active");
             cancelBtn.classList.add("active");
 
-            if(searchInput.value != "" ){
+            // "  n  " => "n"
+            if(searchInput.value.trim() != "" ){
                 let values = searchInput.value;
                 searchData.classList.remove("active");
-                searchData.innerHTML = "<img src='./../assets/images/oso.gif' alt='orange' height='100px' width='100px'>"  + "Sorry, there were no results for " + "&quot;" + values + "&quot;";
+                searchData.innerHTML = `<img src='./../assets/images/oso.gif' alt='orange' height='100px' width='100px'>"
+                  + "Sorry, there were no results for " + "&quot;" + values + "&quot;`;
             } else{
-                searchData.innerHTML = ""
+                const filteredBikes = bikes.filter(obj=> obj.bikeName.contains(searchInput.value.trim()));
+                //todo foreach bikes = table
+                searchData.innerHTML = "";
             }
         }
+
         cancelBtn.onclick = () =>{
             searchBox.classList.remove("active");
             searchInput.classList.remove("active");
@@ -57,5 +65,7 @@
         }
 
 
-
     }
+    
+
+    init();
--- assets/js/login.js
+++ assets/js/login.js
@@ -3,6 +3,8 @@ function submitHandler(event) {
 
 
     event.preventDefault();
+
+    //step 1: 
     let loginMail = document.getElementById("mail").value;                //input from user
     let loginPassword = document.getElementById("password").value;         //input from user
 
@@ -10,6 +12,7 @@ function submitHandler(event) {
     console.log(loginPassword);
 
 
+    //customerDetails (or) customerList
     let customerDetail = JSON.parse(localStorage.getItem("customerList"));       //data from local storage
 
 
@@ -20,6 +23,7 @@ function submitHandler(event) {
 
        let userExist =  false;
         for (let i = 0; i < len; i++) {
+            //let obj = customerDetail[i];
             let mail = customerDetail[i].email;
             let pass = customerDetail[i].password;
 
@@ -34,14 +38,21 @@ function submitHandler(event) {
             } 
 
         } if(userExist){
-            localStorage.setItem("loginDetails",loginMail)
+            //const userObj = { email: "n@gmail.com", role:"USER", name:"N"}; /// Welcome ${name} ; if (user.role)=='ROLE' then show Add Bike
+            //JSON.stringify(userObj)
+            //Avoid storing password for the loggedin user
+
+            localStorage.setItem("loginDetails",loginMail)   //==> loginDetails: "n@gmail.com"
+            //
             alert("logged in successfully");
+            //if(user.role == "USER") then go to ""bike.html
             window.location.href ="./../index.html";
         } else{
-            alert("invalid user id and password");
+            alert("invalid user id / password");
             return null;
         }
 
+        // const userExist = [].some(obj=> obj.email == loginMail && obj.password == loginPassword)
 
 
     }
